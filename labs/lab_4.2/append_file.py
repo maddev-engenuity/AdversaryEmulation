3e402fd7edff58e9f08e0339f62a4e78f650ca32
Govardhen Arunagiri <garunagiri@mitre.org>
2021-08-11 16:22:56 -0400

    added useful output to the rest of the python files

--- labs/lab_4.2/append_file.py
+++ labs/lab_4.2/append_file.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 import argparse
+import sys
 
 
 def auto_int(x):
@@ -8,6 +9,9 @@ def auto_int(x):
 
 
 def append_file(source, dest, seek=None):
+	"""
+	This function appends the contents of the source file into the dest file. If a seek value is specified, 0s are inserted from the end of the dest file until the seek value is reached.
+	"""
 
 	with open(source, 'rb') as in_file:
 		data = in_file.read()
@@ -15,6 +19,9 @@ def append_file(source, dest, seek=None):
 	with open(dest, 'ab') as out_file:
 		if seek:
 			seek_length = seek - out_file.tell()
+			if seek_length < 0:
+				print('[-] Error: The seek offset must be greater than the length of the destination file.')
+				sys.exit(1)
 			out_file.write(bytearray((chr(0) * seek_length).encode('utf-8')))
 
 		print(source + ' start byte is: 0x%0.8x' % out_file.tell())
@@ -33,7 +40,10 @@ def parse_arguments():
 
 def main():
 	args = parse_arguments()
+	task_message = '[+] Appending the contents of {} to {}'.format(args.source, args.dest) + (args.seek ? ' at offset {}'.format(hex(args.seek)) : '')
+	print(task_message)
 	append_file(args.source, args.dest, args.seek)
+	print('[+] Append completed successfully')
 
 
 if __name__ == '__main__':
