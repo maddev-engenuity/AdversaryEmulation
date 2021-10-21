ce8a4732bdbc815242add586143ab8f8a316ccec
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-21 10:29:18 -0700

    added AD import to adddomainusers script

--- vm_setup_scripts/lab1/adddomainusers.ps1
+++ vm_setup_scripts/lab1/adddomainusers.ps1
@@ -1,3 +1,6 @@
+Add-WindowsFeature RSAT-AD-PowerShell
+Import-Module ActiveDirectory
+
 New-ADUser -Name "Jeniffer Tarantino" -GivenName "Jennifer" -Surname "Tarantino" -SamAccountName "jtarantino" -UserPrincipalName "jtarantino@FINANCIAL.local" -Path "OU=Users,DC=FINANCIAL,DC=local" -AccountPassword "jtPassw0rd!" -Enabled $true
 New-ADUser -Name "Donald Dougherty" -GivenName "Donald" -Surname "Dougherty" -SamAccountName "ddougherty" -UserPrincipalName "ddougherty@FINANCIAL.local" -Path "OU=Users,DC=FINANCIAL,DC=local" -AccountPassword "ddPassw0rd!" -Enabled $true
 New-ADUser -Name "Evelyn Gismond" -GivenName "Evelyn" -Surname "Gismond" -SamAccountName "egismond" -UserPrincipalName "egismond@FINANCIAL.local" -Path "OU=Users,DC=FINANCIAL,DC=local" -AccountPassword "egPassw0rd!" -Enabled $true
