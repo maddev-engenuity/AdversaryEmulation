2a9d347296747b58af07d36ceea96cf87c8d0100
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-21 10:01:11 -0700

    Separated static ip script

--- vm_setup_scripts/lab1/createbankdomain.ps1
+++ vm_setup_scripts/lab1/createbankdomain.ps1
@@ -1,17 +1,3 @@
-#Setting static IP within current CIDR range
-$adapter = Get-NetAdapter | ? {$_.Name -eq "Ethernet"}
-$currIP = ($adapter | Get-NetIPAddress -AddressFamily IPv4).IPAddress
-$staticIP = ($currIP.Split(".")[0..2] + 50) -join "."
-
-If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
- $adapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
-}
-If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
- $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
-}
-
-$adapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $staticIP -PrefixLength 24
-
 Install-WindowsFeature AD-Domain-Services
 Import-Module ADDSDeployment
 $SecPassword = ConvertTo-SecureString "j4yqN3EYcL_KQ?L2" -AsPlainText -Force
