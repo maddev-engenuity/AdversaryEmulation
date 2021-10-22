6c42cafdba61232609c4d80f0b1f3459594bc753
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-22 15:38:49 -0400

    more minor changes

--- vm_setup_scripts/lab1/add-domain-entities.ps1
+++ vm_setup_scripts/lab1/add-domain-entities.ps1
@@ -6,14 +6,14 @@ Write-Host "[i] Adding workstations (computer accounts) to domain"
 New-ADComputer -Name "WKST01" -AccountPassword (ConvertTo-SecureString 'wk1Passw0rd!' -AsPlainText -Force)
 New-ADComputer -Name "WKST02" -AccountPassword (ConvertTo-SecureString 'wk2Passw0rd!' -AsPlainText -Force)
 New-ADComputer -Name "WKST03" -AccountPassword (ConvertTo-SecureString 'wk3Passw0rd!' -AsPlainText -Force)
-Write-Host "[i] WKST01, WKST02, and WKST03 computer accounts added to MAD.local"
+Write-Host "[+] WKST01, WKST02, and WKST03 computer accounts added to MAD.local"
 
 #Creating Organizational Units
 Write-Host "[i] Adding Organizational Units to domain"
 New-ADOrganizationalUnit -Name "Managers" -Path "DC=MAD,DC=local"
 New-ADOrganizationalUnit -Name "HR" -Path "DC=MAD,DC=local"
 New-ADOrganizationalUnit -Name "UserAccounts" -Path "DC=MAD,DC=local"
-Write-Host "[i] Managers, HR, and UserAccounts Organizational Units added to MAD.local"
+Write-Host "[+] Managers, HR, and UserAccounts Organizational Units added to MAD.local"
 
 #Creating Users
 Write-Host "[i] Adding Users to domain"
@@ -21,4 +21,4 @@ New-ADUser -Name "Jeniffer Tarantino" -GivenName "Jennifer" -Surname "Tarantino"
 New-ADUser -Name "Donald Dougherty" -GivenName "Donald" -Surname "Dougherty" -SamAccountName "ddougherty" -UserPrincipalName "ddougherty@MAD.local" -Path "OU=HR,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "ddPassw0rd!" -AsPlainText -Force) -Enabled $true
 New-ADUser -Name "Evelyn Gismond" -GivenName "Evelyn" -Surname "Gismond" -SamAccountName "egismond" -UserPrincipalName "egismond@MAD.local" -Path "OU=UserAccounts,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "egPassw0rd!" -AsPlainText -Force) -Enabled $true
 New-ADUser -Name "Shanon Blue" -GivenName "Shanon" -Surname "Blue" -SamAccountName "sblue" -UserPrincipalName "sblue@MAD.local" -Path "OU=UserAccounts,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "sbPassw0rd!" -AsPlainText -Force) -Enabled $true
-Write-Host "[i] jtarantino, ddougherty, egismond, and sblue user accounts added to MAD.local"
+Write-Host "[+] jtarantino, ddougherty, egismond, and sblue user accounts added to MAD.local"
