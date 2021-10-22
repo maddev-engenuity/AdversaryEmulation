351a2000f1695c804d9d4c0adf1c41b7ca430299
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-22 15:20:16 -0400

    specifying extraction location for adfind

--- vm_setup_scripts/lab1/download-tools.ps1
+++ vm_setup_scripts/lab1/download-tools.ps1
@@ -1,7 +1,7 @@
 #Download adfind
 $postParams = @{B1='Download+Now';download="AdFind.zip";email=''}
 Invoke-WebRequest -Uri http://www.joeware.net/downloads/dl.php -Method POST -Body $postParams -OutFile C:\Users\Public\adfind.zip
-Expand-Archive C:\Users\Public\adfind.zip
+Expand-Archive -Path C:\Users\Public\adfind.zip -DestinationPath C:\Users\Public
 
 #Download plink
 Invoke-WebRequest -Uri https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe -OutFile C:\Users\Public\plink.exe
