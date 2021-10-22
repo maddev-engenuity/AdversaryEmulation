277921e03e6108fa07d15055f336068bfa5e2c63
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-22 15:27:55 -0400

    removing unnecessary files

--- vm_setup_scripts/lab1/disabledefenderdomain.ps1
+++
@@ -1,4 +0,0 @@
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"
-New-GPO -Name "Disable Windows Defender" -Comment "Disables Windows Defender for the Domain"
-Set-GPRegistryValue -Name "Disable Windows Defender" -Key "HKCU\Software\Policies\Microsoft\Windows Defender" -ValueName DisableAntiSpyware -Type REG_DWORD -Value 1
-New-GPLink -Name "Disable Windows Defender" -Target "dc=FINANCIAL,dc=local"
