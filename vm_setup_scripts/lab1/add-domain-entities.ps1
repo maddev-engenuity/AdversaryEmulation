6796b9ddf041e208c08c107979724c29323c428e
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-22 13:50:46 -0400

    added comments and console output

--- vm_setup_scripts/lab1/add-domain-entities.ps1
+++ vm_setup_scripts/lab1/add-domain-entities.ps1
@@ -1,15 +1,24 @@
 Add-WindowsFeature RSAT-AD-PowerShell
 Import-Module ActiveDirectory
 
+#Creating workstations in the form of Computer Accounts
+Write-Host "[i] Adding workstations (computer accounts) to domain"
 New-ADComputer -Name "WKST01" -AccountPassword (ConvertTo-SecureString 'wk1Passw0rd!' -AsPlainText -Force)
 New-ADComputer -Name "WKST02" -AccountPassword (ConvertTo-SecureString 'wk2Passw0rd!' -AsPlainText -Force)
 New-ADComputer -Name "WKST03" -AccountPassword (ConvertTo-SecureString 'wk3Passw0rd!' -AsPlainText -Force)
+Write-Host "[i] WKST01, WKST02, and WKST03 computer accounts added to MAD.local"
 
+#Creating Organizational Units
+Write-Host "[i] Adding Organizational Units to domain"
 New-ADOrganizationalUnit -Name "Managers" -Path "DC=MAD,DC=local"
 New-ADOrganizationalUnit -Name "HR" -Path "DC=MAD,DC=local"
 New-ADOrganizationalUnit -Name "UserAccounts" -Path "DC=MAD,DC=local"
+Write-Host "[i] Managers, HR, and UserAccounts Organizational Units added to MAD.local"
 
+#Creating Users
+Write-Host "[i] Adding Users to domain"
 New-ADUser -Name "Jeniffer Tarantino" -GivenName "Jennifer" -Surname "Tarantino" -SamAccountName "jtarantino" -UserPrincipalName "jtarantino@MAD.local" -Path "OU=Managers,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "jtPassw0rd!" -AsPlainText -Force) -Enabled $true
 New-ADUser -Name "Donald Dougherty" -GivenName "Donald" -Surname "Dougherty" -SamAccountName "ddougherty" -UserPrincipalName "ddougherty@MAD.local" -Path "OU=HR,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "ddPassw0rd!" -AsPlainText -Force) -Enabled $true
 New-ADUser -Name "Evelyn Gismond" -GivenName "Evelyn" -Surname "Gismond" -SamAccountName "egismond" -UserPrincipalName "egismond@MAD.local" -Path "OU=UserAccounts,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "egPassw0rd!" -AsPlainText -Force) -Enabled $true
 New-ADUser -Name "Shanon Blue" -GivenName "Shanon" -Surname "Blue" -SamAccountName "sblue" -UserPrincipalName "sblue@MAD.local" -Path "OU=UserAccounts,DC=MAD,DC=local" -AccountPassword (ConvertTo-SecureString "sbPassw0rd!" -AsPlainText -Force) -Enabled $true
+Write-Host "[i] jtarantino, ddougherty, egismond, and sblue user accounts added to MAD.local"
--- vm_setup_scripts/lab1/rename-dc.ps1
+++ vm_setup_scripts/lab1/rename-dc.ps1
@@ -1,2 +1,4 @@
 #Rename Computer, takes effect on restart
+Write-Host "[i] Renaming server to DC01"
+Write-Host "[i] Server will automatically restart"
 Rename-Computer -NewName "DC01" -Restart
