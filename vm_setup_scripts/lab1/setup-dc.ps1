e015391acb2098e82c22c6a1f7db1ca0ccf36c81
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-22 15:16:23 -0400

    autologon feature

--- vm_setup_scripts/lab1/setup-dc.ps1
+++ vm_setup_scripts/lab1/setup-dc.ps1
@@ -6,6 +6,13 @@ if ($env:COMPUTERNAME -ne "DC01") {
     #Disable defender, add setup script to registry run key, and rename computer.
     powershell -ep bypass C:\Users\Administrator\AdversaryEmulation\vm_setup_scripts\lab1\disable-defender.ps1;
 
+    #Setting up autologon to make setup process simpler and easier for students
+    Write-Host "[i] If you wish to avoid having to manually login each time the server reboots during the setup process, please provide your password here. Otherwise, simply leave blank and hit enter."
+    $password = Read-Host "Login Password:"
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultUserName' -Type String -Value "Administrator";
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultPassword' -Type String -Value $password;
+    New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'AutoAdminLogon' -Type String -Value "1";
+
     Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name 'SetupDC' -Value "C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noexit -ep bypass C:\Users\Administrator\AdversaryEmulation\vm_setup_scripts\lab1\setup-dc.ps1"
     Write-Host "[i] Set registry Run key for this script. Script will automatically complete with intermittent reboots"
     Start-Sleep -Seconds 3
@@ -13,6 +20,9 @@ if ($env:COMPUTERNAME -ne "DC01") {
 } 
 #Step 2
 elseif ((Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem).Domain -ne "MAD.local") {
+    #Add domain name to auto login
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultDomainName' -Type String -Value "MAD";
+
     #Create MAD.local domain. Server will restart after domain is created
     powershell -ep bypass C:\Users\Administrator\AdversaryEmulation\vm_setup_scripts\lab1\create-domain.ps1;
 }
@@ -26,7 +36,16 @@ else {
 
     #Remove setup script from registry Run key
     Remove-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name 'SetupDC'
-    Write-Host "[i] Registry Run key for this script removed. Setup is now complete. Server will reboot for clean start."
+    Write-Host "[i] Registry Run key for this script removed."
+
+    #Remove Autologon creds
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultUserName' -Type String -Value "";
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultPassword' -Type String -Value "";
+    Remove-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'AutoAdminLogon';
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name 'DefaultDomainName' -Type String -Value "";
+    Write-Host "[i] Removed autologon credentials."
+
+    Write-Host "[i] Setup is now complete. Server will reboot for clean start."
     Start-Sleep -Seconds 3
     Restart-Computer -Force
 }
