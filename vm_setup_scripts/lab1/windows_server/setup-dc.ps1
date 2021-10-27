dd1709fc1444cc70590260d9c2b14cbc6c2445f5
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-27 00:05:29 -0400

    missed an underscore, turned it into a dash

--- vm_setup_scripts/lab1/windows_server/setup-dc.ps1
+++ vm_setup_scripts/lab1/windows_server/setup-dc.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 #Step 0
 #Copy scripts to C:\Users\Public if they're not already there
-if (-not (Test-Path C:\Users\Public\setup-dc.ps1) -or -not (Test-Path C:\Users\Public\set-windows-wallpaper.ps1) -or -not (Test-Path C:\Users\Public\Pictures\target_background.png)) {
+if (-not (Test-Path C:\Users\Public\setup-dc.ps1) -or -not (Test-Path C:\Users\Public\set-windows-wallpaper.ps1) -or -not (Test-Path C:\Users\Public\Pictures\target-background.png)) {
     Write-Host "[i] Copying scripts to C:\Users\Public";
     Copy-Item .\setup-dc.ps1,.\disable-defender.ps1,.\rename-dc.ps1,.\create-domain.ps1,.\add-domain-entities.ps1,.\download-tools.ps1,.\set-windows-wallpaper.ps1 -Destination C:\Users\Public;
     Copy-Item .\target-background.png -Destination C:\Users\Public\Pictures\target-background.png;
