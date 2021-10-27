573034d0f38e1fa0ca6e377a41b09d39e9566a93
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-27 09:30:26 -0400

    typo

--- vm_setup_scripts/lab1/windows_server/setup-dc.ps1
+++ vm_setup_scripts/lab1/windows_server/setup-dc.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = "Stop"
 #Copy scripts to C:\Users\Public if they're not already there
 if (-not (Test-Path C:\Users\Public\setup-dc.ps1) -or -not (Test-Path C:\Users\Public\set-windows-wallpaper.ps1) -or -not (Test-Path C:\Users\Public\Pictures\target-background.png)) {
     Write-Host "[i] Copying scripts to C:\Users\Public";
-    Copy-Item .\setup-dc.ps1,.\disable-defender.ps1,.\rename-dc.ps1,.\create-domain.ps1,.\add-domain-entities.ps1,.\download-emulation-executables.ps1,.\install-detection-tools,.\hidden-files,.\set-windows-wallpaper.ps1 -Destination C:\Users\Public;
+    Copy-Item .\setup-dc.ps1,.\disable-defender.ps1,.\rename-dc.ps1,.\create-domain.ps1,.\add-domain-entities.ps1,.\download-emulation-executables.ps1,.\install-detection-tools.ps1,.\hidden-files.ps1,.\set-windows-wallpaper.ps1 -Destination C:\Users\Public;
     Copy-Item .\target-background.png -Destination C:\Users\Public\Pictures\target-background.png;
 }
 
