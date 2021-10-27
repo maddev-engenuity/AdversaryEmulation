8db6208b9c518c2238617d69910242b8e7680c37
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-27 10:02:15 -0400

    removing ssl bypass attempts, they don't work

--- vm_setup_scripts/lab1/windows_server/install-detection-tools.ps1
+++ vm_setup_scripts/lab1/windows_server/install-detection-tools.ps1
@@ -1,33 +1,10 @@
-# If Chocolatey package manager is not installed already, install
-if (-not (powershell choco -v)) {
-    Write-Host "[*] Installing Chocolatey package manager"
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    try {
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-    } catch {
-        # [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true} ;
-        add-type @"
-        using System.Net;
-        using System.Security.Cryptography.X509Certificates;
-        public class TrustAllCertsPolicy : ICertificatePolicy {
-            public bool CheckValidationResult(
-                ServicePoint srvPoint, X509Certificate certificate,
-                WebRequest request, int certificateProblem) {
-                return true;
-            }
-        }
-"@;
-        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy;
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-    }
-}
-
-# install needed tools
-Write-Host "[*] Installing needed tools"
+#Install Chocolatey package manager, and then install sysinternals, apimonitor, and wireshark
+Write-Host "[*] Installing Chocolatey package manager"
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 try {
-    choco install sysinternals apimonitor wireshark -y
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+    choco install sysinternals apimonitor wireshark -y;
 } catch {
-    Write-Host "[-] It looks like your VM is behind a proxy causing SSL Certificate errors. To run this script, either connect to the internet without the proxy, or install the proxy's certificates on your VM. Once that's done, you can either rerun this script, or simply run the following command: "
-    Write-Host "[i] choco install sysinternals apimonitor wireshark -y"
+    Write-Host "[-] It looks like your VM may be behind a proxy causing SSL Certificate errors. To run this script, either connect to the internet without the proxy, or install the proxy's certificates on your VM. Once that's done, you can rerun this script."
 }
