1e2a4f4ae80ee5ab83deed6a1a680577aecf7602
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-29 01:04:04 -0400

    removing need for user input from install-dependencies.sh

--- vm_setup_scripts/lab1/kali/install-dependencies.sh
+++ vm_setup_scripts/lab1/kali/install-dependencies.sh
@@ -2,9 +2,9 @@
 
 echo "[i] Installing Pip3"
 apt update
-apt install python3-pip
+apt install -y python3-pip
 
 echo "[i] Installing PyLNK3 version 0.4.2"
-su -c "pip3 install -Iv pylnk3==0.4.2" attacker
+su -c "pip3 install --yes -Iv pylnk3==0.4.2" attacker
 
 echo "[i] Dependencies installed"
