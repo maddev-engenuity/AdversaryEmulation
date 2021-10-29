d38432494879c3e6cb10a1f80d4f0656f1a3645f
Govardhen Arunagiri <garunagiri@mitre.org>
2021-10-29 01:59:36 -0400

    changing wallpaper script for running by local user

--- vm_setup_scripts/lab1/kali/set-kali-wallpaper.sh
+++ vm_setup_scripts/lab1/kali/set-kali-wallpaper.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 echo "[*] Setting background image for attacker user"
-
-image_path=$(pwd)
-
-uid=$(id -u attacker)
-dbus="unix:path=/run/user/$uid/bus"
-
-su -c "env DBUS_SESSION_BUS_ADDRESS=$dbus xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s $image_path/attacker-background.png" attacker
-
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s /home/attacker/AdversaryEmulation/vm_setup_scripts/lab1/kali/attacker-background.png
